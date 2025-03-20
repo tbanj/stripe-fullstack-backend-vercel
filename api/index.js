4bd35b0a2c227054a3ac64de7c7c2fb53731c75e
MAC <mac@MACs-MacBook-Pro.local>
2025-03-21 00:43:50 +0100

    express.js project hosted on vercel

--- api/index.js
+++ api/index.js
@@ -9,29 +9,5 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-// const express = require('express');
-// const port = process.env.PORT || 3001;
-// const app = express();
-// const { sql } = require('@vercel/postgres');
-
-// const bodyParser = require('body-parser');
-// const path = require('path');
-
-// // Create application/x-www-form-urlencoded parser
-// const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-// app.use(express.static('public'));
-
-// app.get('/', function (req, res) {
-// 	res.status(201).json({
-//         data: "Welcome to Stripe Homepage",
-//         message: "Root stripe fullstack is called Successfully",
-//       });
-// });
-
-
-// app.listen(port, () => console.log(`Server ready on port ${port}.`));
-
-// module.exports = app;
 
 
